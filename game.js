65a69db4c09b507cba5545a7d766562d126c611e
Ivan Portianoy <iportianoy@gmail.com>
2017-11-18 13:28:29 +0200

    Changed form of asteroids to triangle

--- game.js
+++ game.js
@@ -642,23 +642,15 @@ AlienBullet.prototype = new Bullet();
 
 Asteroid = function () {
   this.init("asteroid",
-      [-10,   0,
-        -5,   7,
-        -3,   4,
-        1,  10,
-        5,   4,
-        10,   0,
-        5,  -6,
-        2, -10,
-        -4, -10,
-        -4,  -5]);
-
+            [-10,   0,
+              -5,   7,
+              -3,   4]);
   this.visible = true;
   this.scale = 6;
   this.postMove = this.wrapPostMove;
 
   this.collidesWith = ["ship", "bullet", "bigalien", "alienbullet"];
-
+    
   this.collision = function (other) {
     SFX.explosion().play();
     if (other.name == "bullet") Game.score += 120 / this.scale;
